88945e8440898be01b66c68fbf71a57733323156
phanuphanthcanthrsngsaeng17-del <phanuphanthcanthrsngsaeng17@gmail.com>
2026-08-23 16:14:08 +0700

    Move tools endpoint out of Vercel serverless functions

--- api/tools.js
+++
@@ -1,8 +0,0 @@
-import { getToolRegistry } from './boss-tools.js';
-import { publicTool } from './security.js';
-
-export default function handler(req,res) {
-  res.setHeader('Cache-Control','no-store');
-  if (req.method !== 'GET') { res.setHeader('Allow','GET'); return res.status(405).json({error:'Method Not Allowed'}); }
-  return res.status(200).json({ ok:true, tools:getToolRegistry().map(publicTool) });
-}
